8545b751c48eac51bc46e858b4ef4c267d980147
Hugh <197298026+HueCodes@users.noreply.github.com>
2026-01-13 22:58:13 -0800

    Add Objective-C bridging header for C solver

---
+++ ios/SudokuVision/Utilities/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  SudokuVision
+//
+//  Bridging header for C code integration
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#include "sudoku.h"
+
+#endif /* BridgingHeader_h */
